42673ac5e7fd4292ffb00a899d4d3ff2c58a8b88
김보운 <haioer@naver.com>
2017-02-06 00:06:58 +0900

    Update CognitiveAPI.php

--- app/Http/Controllers/Cognitive/CognitiveAPI.php
+++ app/Http/Controllers/Cognitive/CognitiveAPI.php
@@ -15,11 +15,6 @@ require_once 'HTTP/Request2.php';
 
 class CognitiveAPI extends Controller
 {
-    public function index()
-    {
-        return view('cognitive/cognitive');
-    }
-
     public function showText()
     {
         $request = new \Http_Request2('https://api.projectoxford.ai/vision/v1.0/ocr');
@@ -38,7 +33,6 @@ class CognitiveAPI extends Controller
         $url->setQueryVariables($parameters);
         $request->setMethod(\HTTP_Request2::METHOD_POST);
         // Request body
-//        $urlString = 'https://miris.blob.core.windows.net/images/' . '1485183837.jpg';
         $urlString = FileController::getImageUrl();
         $urlData = array('url' => $urlString);
         $urlDataJsonEncode = json_encode($urlData);
@@ -66,7 +60,6 @@ class CognitiveAPI extends Controller
         $url->setQueryVariables($parameters);
         $request->setMethod(\HTTP_Request2::METHOD_POST);
         // Request body
-//        $urlString = 'https://miris.blob.core.windows.net/images/' . '1485183837.jpg
         $urlString = FileController::getImageUrl();
         $urlData = array('url' => $urlString);
         $urlDataJsonEncode = json_encode($urlData);
@@ -75,6 +68,45 @@ class CognitiveAPI extends Controller
             $response = $request->send();
             echo $response->getBody();
         } catch (HttpException $ex) {
+            echo $ex;
+        }
+    }
+    
+    public function showDescription() {
+        $request = new \Http_Request2('https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze');
+        $url = $request->getUrl();
+
+        $headers = array(
+        // Request headers
+            'Content-Type' => 'application/json',
+            'Ocp-Apim-Subscription-Key' => env('COGNITIVE_VISION_KEY'),
+        );
+
+        $request->setHeader($headers);
+
+        $parameters = array(
+            // Request parameters
+            'visualFeatures' => 'Description',
+            'language' => 'en',
+        );
+
+        $url->setQueryVariables($parameters);
+
+        $request->setMethod(HTTP_Request2::METHOD_POST);
+
+        // Request body
+        $urlString = FileController::getImageUrl();
+        $urlData = array('url' => $urlString);
+        $urlDataJsonEncode = json_encode($urlData);
+        $request->setBody($urlDataJsonEncode);
+
+        try
+        {
+            $response = $request->send();
+            echo $response->getBody();
+        }
+        catch (HttpException $ex)
+        {
             echo $ex;
         }
     }
