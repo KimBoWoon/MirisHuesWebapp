e6429d8e40adcff444e4fc17710b0daa1baf30e3
KimBoWoon <haioer@naver.com>
2017-03-02 11:54:48 +0900

    json add

--- app/Http/Controllers/Google/GoogleVisionAPI.php
+++ app/Http/Controllers/Google/GoogleVisionAPI.php
@@ -19,11 +19,9 @@ class GoogleVisionAPI
     public function getText()
     {
         $client = new Google_Client();
-        $client->setAuthConfig(__DIR__ . '/miris_client_secret.json');
-        $client->setAccessType("online");
-        $client->authorize(new Client(array(
-            'verify' => __DIR__ . '/cacert.pem',
-        )));
+        $client->setAuthConfig(__DIR__ . '/miris-vision.json');
+        $client->setAccessType("offline");
+        $client->authorize(new Client(['verify' => false]));
         $client->addScope(Google_Service_Storage::DEVSTORAGE_FULL_CONTROL);
         $client->setApplicationName("miris");
         $client->setDeveloperKey(env('GOOGLE_CLIENT_KEY'));
