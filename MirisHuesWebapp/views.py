321ca1efec510555ac496e3c6b081c9dd59e5a0e
KimBoWoon <haioer@naver.com>
2017-03-11 01:03:25 +0900

    modify

---
+++ MirisHuesWebapp/views.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from flask import render_template
+from FlaskWebProject import app
+
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
