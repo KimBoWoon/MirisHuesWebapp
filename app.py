02fb846592706f7f30e68c1ef524a57fc2e1f25b
KimBoWoon <haioer@naver.com>
2017-03-13 14:03:16 +0900

    modify WSGI_ALT_VIRTUALENV_HANDLER

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-def wsgi_app(environ, start_response):
-    status = '200 OK'
-    response_headers = [('Content-type', 'text/plain')]
-    start_response(status, response_headers)
-    response_body = 'Hello World'
-    yield response_body.encode()
+from os import environ
+from MirisHuesWebapp import app
 
 if __name__ == '__main__':
-    from wsgiref.simple_server import make_server
-
-    httpd = make_server('localhost', 5555, wsgi_app)
-    httpd.serve_forever()
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT, debug=True)
