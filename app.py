d76089038165048c737e11ca74fa583864ba02a1
KimBoWoon <haioer@naver.com>
2017-03-26 17:01:11 +0900

    modify app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from os import environ
 from MirisHuesWebapp import app
 
-HOST = environ.get('SERVER_HOST', 'localhost')
-try:
-    PORT = int(environ.get('SERVER_PORT', '5555'))
-except ValueError:
-    PORT = 5555
-app.run(HOST, PORT, debug=True)
+if __name__ == '__main__':
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT, debug=True)
