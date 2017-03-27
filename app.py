96b7bd3e7b66f5040ad1b4c676e0f599f2caf62e
KimBoWoon <haioer@naver.com>
2017-03-28 02:43:46 +0900

    finish

--- app.py
+++ app.py
@@ -1,17 +1,7 @@
 from MirisHuesWebapp import app
-from flask_restful import reqparse, Resource, Api
-
-api = Api(app)
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
 wsgi_app = app.wsgi_app
-
-
-@app.route('/')
-def index():
-    """Renders home page."""
-    return "Python Flask is up and running!"
-
 
 if __name__ == '__main__':
     import os
