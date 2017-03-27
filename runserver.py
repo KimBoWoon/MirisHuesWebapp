e506d77514454667684c98b327dcc3e2d60483e9
KimBoWoon <haioer@naver.com>
2017-03-28 02:20:19 +0900

    modify runserver.py

--- runserver.py
+++ runserver.py
@@ -1,9 +1,11 @@
 from MirisHuesWebapp import app
+from werkzeug.contrib.fixers import ProxyFix
 
-wsgi_app = app.wsgi_app
+# wsgi_app = app.wsgi_app
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
 
-    httpd = make_server('localhost', 5555, wsgi_app)
+    httpd = make_server('localhost', 5555, app.wsgi_app)
     httpd.serve_forever()
