2552f226f0eb59a1d03d7fa36acba3ffa45db5f9
KimBoWoon <haioer@naver.com>
2017-03-28 01:35:02 +0900

    requirements modify

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+from tornado.stack_context import NullContext
+from tornado.wsgi import WSGIContainer
+from MirisHuesWebapp import app
 from os import environ
 from gevent.wsgi import WSGIServer
 from MirisHuesWebapp import app
-
-wsgi_app = app.wsgi_app
 
 if __name__ == '__main__':
     # HOST = environ.get('SERVER_HOST', 'localhost')
@@ -12,11 +15,8 @@ if __name__ == '__main__':
     #     PORT = 5555
     # app.run(HOST, PORT, debug=True)
 
-    http_server = WSGIServer(('localhost', 5555), app)
-    http_server.serve_forever()
-
-    # http_server = HTTPServer(WSGIContainer(app))
-    # with NullContext():
-    #     http_server.bind(port=5555, address="127.0.0.1")
-    #     http_server.start(6)
-    # IOLoop.instance().start()
+    http_server = HTTPServer(WSGIContainer(app))
+    with NullContext():
+        http_server.bind(port=5555, address="127.0.0.1")
+        http_server.start(6)
+    IOLoop.instance().start()
