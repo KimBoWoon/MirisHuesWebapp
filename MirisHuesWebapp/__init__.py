9fce8bb5a8060e828349b1828209642b92e69945
KimBoWoon <haioer@naver.com>
2017-03-28 01:30:39 +0900

    WSGIServer

--- MirisHuesWebapp/__init__.py
+++ MirisHuesWebapp/__init__.py
@@ -1,5 +1,5 @@
-# from flask import Flask
+from flask import Flask
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# import MirisHuesWebapp.views
+import MirisHuesWebapp.views
--- app.py
+++ app.py
@@ -2,22 +2,23 @@ from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from tornado.stack_context import NullContext
 from tornado.wsgi import WSGIContainer
-# from MirisHuesWebapp import app
+from MirisHuesWebapp import app
 from os import environ
-
-from flask import Flask
-
-app = Flask(__name__)
+from gevent.wsgi import WSGIServer
+from MirisHuesWebapp import app
 
 wsgi_app = app.wsgi_app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT, debug=True)
+    # HOST = environ.get('SERVER_HOST', 'localhost')
+    # try:
+    #     PORT = int(environ.get('SERVER_PORT', '5555'))
+    # except ValueError:
+    #     PORT = 5555
+    # app.run(HOST, PORT, debug=True)
+
+    http_server = WSGIServer(('localhost', 5555), app)
+    http_server.serve_forever()
 
     # http_server = HTTPServer(WSGIContainer(app))
     # with NullContext():
