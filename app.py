07c8518a3ba9ea2f122756908fadf67f6f186878
KimBoWoon <haioer@naver.com>
2017-03-28 01:24:15 +0900

    modify __init__.py

--- app.py
+++ app.py
@@ -2,19 +2,22 @@ from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from tornado.stack_context import NullContext
 from tornado.wsgi import WSGIContainer
-from MirisHuesWebapp import app
+# from MirisHuesWebapp import app
 from os import environ
+
+from flask import Flask
+
+app = Flask(__name__)
 
 wsgi_app = app.wsgi_app
 
 if __name__ == '__main__':
-    app.run()
-    # HOST = environ.get('SERVER_HOST', 'localhost')
-    # try:
-    #     PORT = int(environ.get('SERVER_PORT', '5555'))
-    # except ValueError:
-    #     PORT = 5555
-    # app.run(HOST, PORT, debug=True)
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT, debug=True)
 
     # http_server = HTTPServer(WSGIContainer(app))
     # with NullContext():
